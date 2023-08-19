ec5166cd0c1646d231602172fdf3442cc9cd5b41
junaeidy <140931686+junaeidy@users.noreply.github.com>
2023-08-20 00:31:38 +0700

    add Tugas CRUD

---
+++ crud/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perpustakaan | Home</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+</head>
+<body>
+<center>
+    <hr>
+    <a href="../crud/" class="btn btn-outline-primary">Home</a> |
+    <a href="../crud/buku/" class="btn btn-outline-primary">Buku</a> |
+    <a href="../crud/penerbit/" class="btn btn-outline-primary">Penerbit</a> |
+    <a href="../crud/pengarang/" class="btn btn-outline-primary">Pengarang</a> |
+    <a href="../crud/katalog/" class="btn btn-outline-primary">Katalog</a>
+    <hr>
+</center>
+
+<h3>
+    Selamat datang di Perpustakaan CRUD
+</h3>
+</body>
+</html>
---
+++ crud/katalog/add.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+	<title>Add Katalog</title>
+</head>
+
+<?php
+	include_once("connect.php");
+    $penerbit = mysqli_query($mysqli, "SELECT * FROM penerbit");
+    $pengarang = mysqli_query($mysqli, "SELECT * FROM pengarang");
+    $katalog = mysqli_query($mysqli, "SELECT * FROM katalog");
+?>
+ 
+<body>
+	<a href="index.php">Back</a>
+	<br/><br/>
+ 
+	<form action="add.php" method="post" name="form1">
+		<table width="25%" border="0">
+			<tr> 
+				<td>Id Katalog</td>
+				<td><input type="text" name="id_katalog"></td>
+			</tr>
+			<tr> 
+				<td>Nama Katalog</td>
+				<td><input type="text" name="nama_katalog"></td>
+			</tr>
+			<tr> 
+				<td></td>
+				<td><input type="submit" name="Submit" value="Add"></td>
+			</tr>
+		</table>
+	</form>
+	
+	<?php
+	 
+		// Check If form submitted, insert form data into users table.
+		if(isset($_POST['Submit'])) {
+			$id_katalog = $_POST['id_katalog'];
+			$nama_katalog = $_POST['nama_katalog'];
+			
+			include_once("connect.php");
+
+			$result = mysqli_query($mysqli, "INSERT INTO `katalog` (`id_katalog`, `nama_katalog`) VALUES ('$id_katalog', '$nama_katalog');");
+			
+			header("Location:index.php");
+		}
+	?>
+</body>
+</html>
